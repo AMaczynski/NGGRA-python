5a599f43c3bc24b9628704194dd00dedc9cb185c
Piotr <piotrwiklinski@gmail.com>
2019-04-19 23:39:17 +0200

    rewritten almost everything. separate algorithms, ImageProcessor class

---
+++ algoimpl.py
@@ -0,0 +1,52 @@
+from copy import copy
+
+import cv2
+import numpy as np
+
+
+def simple_algorithm(frame, hsv_ranges=None):
+    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    h_range = (0, 80)
+    s_range = (0, 255)
+    v_range = (148, 255)
+    if hsv_ranges is not None:
+        h_range = hsv_ranges[0]
+        s_range = hsv_ranges[1]
+        v_range = hsv_ranges[2]
+    hsv_min = np.array([h_range[0], s_range[0], v_range[0]])
+    hsv_max = np.array([h_range[1], s_range[1], v_range[1]])
+    frame_threshed = cv2.inRange(hsv_image, hsv_min, hsv_max)
+    frame_threshed_closed = cv2.morphologyEx(frame_threshed, cv2.MORPH_CLOSE,
+                                             cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+    return frame_threshed_closed
+
+
+def advanced_algorithm(frame):
+    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    ycrcb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
+    output_img = copy(frame)
+
+    for i in range(0, len(hsv_image)):
+        for j in range(0, len(hsv_image[0])):
+            h = hsv_image[i][j][0] * 2
+            s = hsv_image[i][j][1] / 255
+            b = frame[i][j][0]
+            g = frame[i][j][1]
+            r = frame[i][j][2]
+            y = ycrcb_image[i][j][0]
+            cr = ycrcb_image[i][j][1]
+            cb = ycrcb_image[i][j][2]
+
+            if h <= 50.0 and 0.23 <= s <= 0.68 and r > 95 and g > 40 and b > 20 and r > g and r > b and abs(
+                    r - g) > 15:
+                output_img[i][j] = 255
+            elif r > 95 and g > 40 and b > 20 and r > g and r > b and abs(
+                    r - g) > 15 and cr > 135 and cb > 85 and y > 80 and cr <= ((1.5862 * cb) + 20) and cr >= (
+                    (0.3448 * cb) + 76.2069) and cr >= ((-4.5652 * cb) + 234.5652) and cr <= (
+                    (-1.15 * cb) + 301.75) and cr <= ((-2.2857 * cb) + 432.85):
+                output_img[i][j] = 255
+            else:
+                output_img[i][j] = 0
+
+    return output_img
---
+++ imgprocessor.py
@@ -0,0 +1,73 @@
+import os
+from time import sleep
+
+import cv2
+
+from algoimpl import simple_algorithm, advanced_algorithm
+
+ALGO_SIMPLE = 0
+ALGO_ADV = 1
+
+STATE_WAITING = 0
+STATE_GRABBING = 1
+
+
+class ImageProcessor:
+    def __init__(self, cam, target_scale):
+        self.cam = cam
+        self.target_scale = target_scale
+        self.custom_simple_ranges = None
+
+    def redefine_simple_algorithm(self, hsv_ranges):
+        self.custom_simple_ranges = hsv_ranges
+
+    def start_tensorflow_analyser(self, target_algorithm):
+        while True:
+            processed_image = self.get_processed_image(target_algorithm)
+            # TODO: gesture = tensorflow(processed_image)
+            break
+        self.cam.release()
+        cv2.destroyAllWindows()
+
+    def start_grabber(self, target_algorithm, file_name, grabber_target, grabber_delay):
+        grabber_state = STATE_WAITING
+        img_counter = 0
+        while True:
+            processed_image = self.get_processed_image(target_algorithm)
+            if processed_image is None:
+                break
+            cv2.imshow("Show by CV2", processed_image)
+            if grabber_state == STATE_GRABBING:
+                img_name = "output/%s/%s_%d.png" % (file_name, file_name, img_counter)
+                cv2.imwrite(img_name, processed_image)
+                print("{} written!".format(img_name))
+                img_counter += 1
+                sleep(grabber_delay)
+                if img_counter == grabber_target:
+                    break
+            else:
+                k = cv2.waitKey(1)
+
+                if k % 256 == 27:  # ESC
+                    print("Escape hit, closing...")
+                    break
+                elif k % 256 == 32:  # space
+                    if not os.path.exists("output/" + file_name):
+                        os.makedirs("output/" + file_name)
+                    print("Starting grabbing")
+                    grabber_state = STATE_GRABBING
+        self.cam.release()
+        cv2.destroyAllWindows()
+
+    def get_processed_image(self, target_algorithm):
+        ret, frame = self.cam.read()
+        new_x, new_y = frame.shape[1] * self.target_scale, frame.shape[0] * self.target_scale
+        scaled_img = cv2.resize(frame, (int(new_x), int(new_y)))
+        if not ret:
+            return None
+        processed_image = None
+        if target_algorithm == ALGO_SIMPLE:
+            processed_image = simple_algorithm(scaled_img, self.custom_simple_ranges)
+        elif target_algorithm == ALGO_ADV:
+            processed_image = advanced_algorithm(scaled_img)
+        return processed_image
