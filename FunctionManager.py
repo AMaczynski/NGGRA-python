f106c0c583f3fa33ab6109e6cbbb4ed20e608d71
Mateusz <matteuszz20@gmail.com>
2019-04-23 13:03:48 +0200

    Add change volume level

--- FunctionManager.py
+++ FunctionManager.py
@@ -1,5 +1,5 @@
 
-from pycaw.pycaw import AudioUtilities
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 from comtypes import CLSCTX_ALL, POINTER, cast
 
 def speakersMute():
@@ -10,3 +10,19 @@ def speakersMute():
             volume.SetMute(0, None)
         else:
             volume.SetMute(1, None)
+
+
+
+
+def speakersChangeVolumeLevel(mode):
+    a = 0
+    if(mode == 1):
+        a=0.1
+    else:
+        a=-0.1
+
+    sessions = AudioUtilities.GetAllSessions()
+    for session in sessions:
+        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+        if session.Process and session.Process.name() == "chrome.exe":
+            volume.SetMasterVolume(volume.GetMasterVolume()+a, None)
