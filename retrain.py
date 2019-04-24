b3852bf620408d239dae0205500faf8210823a05
Mateusz <matteuszz20@gmail.com>
2019-04-24 13:17:24 +0200

    set default image-dir

--- retrain.py
+++ retrain.py
@@ -1161,7 +1161,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--image_dir',
       type=str,
-      default='',
+      default='output',
       help='Path to folders of labeled images.'
   )
   parser.add_argument(
