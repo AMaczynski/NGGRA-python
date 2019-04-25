2902b192d4fa96f55f18524b6508d4e793e85f1d
Piotr <piotrwiklinski@gmail.com>
2019-04-25 08:33:00 +0200

    Added feature to capture test cases

--- grabber.py
+++ grabber.py
@@ -8,36 +8,50 @@ ALGO_SIMPLE = 0
 ALGO_ADV = 1
 
 algorithm = ALGO_SIMPLE
-capture_name = "straight"
+capture_name = "none"
 images_to_capture = 10
-capture_delay = 1
+capture_delay = 0.5
 
 save_raw = True
 save_bin = True
 save_cropped_bin = True
+grab_test = True
 
 if __name__ == '__main__':
     cam = cv2.VideoCapture(0)
     # ImageProcessor(cam reference, image scale)
     ip = ImageProcessor(cam, 0.5)
-    dir_path = "output/%s" % capture_name
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    dir_path = "output_cropped/%s" % capture_name
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
     start_number = 0
-    dir_path = "output_raw/%s" % capture_name
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+    if grab_test:
+        dir_path = "test"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        else:
+            files = os.listdir(dir_path)
+            for file in files:
+                number = int(file.split("_")[-1][:-4])
+                if number > start_number:
+                    start_number = number
+            start_number += 1
     else:
-        files = os.listdir(dir_path)
-        for file in files:
-            number = int(file.split("_")[-1][:-4])
-            if number > start_number:
-                start_number = number
-        start_number += 1
+        dir_path = "output/%s" % capture_name
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        dir_path = "output_cropped/%s" % capture_name
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        dir_path = "output_raw/%s" % capture_name
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        else:
+            files = os.listdir(dir_path)
+            for file in files:
+                number = int(file.split("_")[-1][:-4])
+                if number > start_number:
+                    start_number = number
+            start_number += 1
+
     if algorithm is ALGO_SIMPLE:
         # custom_hsv_ranges = ((20, 50), # H
         #                      (0,255), # S
@@ -46,6 +60,6 @@ if __name__ == '__main__':
                              (100, 255),  # S
                              (140, 255))  # V
         ip.redefine_simple_algorithm(custom_hsv_ranges)
-        ip.start_grabber(ALGO_SIMPLE, capture_name, images_to_capture, capture_delay, start_number)
+        ip.start_grabber(ALGO_SIMPLE, capture_name, images_to_capture, capture_delay, start_number, grab_test)
     elif algorithm is ALGO_ADV:
-        ip.start_grabber(ALGO_ADV, capture_name, images_to_capture, capture_delay, start_number)
+        ip.start_grabber(ALGO_ADV, capture_name, images_to_capture, capture_delay, start_number, grab_test)
